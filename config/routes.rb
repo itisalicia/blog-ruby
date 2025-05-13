3eab3fd8d7790215c8a3a0c48417913ffc9955aa
Alicia <alicia.magro@ouidou.fr>
2025-05-13 16:23:19 +0200

    final commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
-  resources :articles
+
+  resources :articles do
+    resources :comments
+  end
 
   # In more details, the resources method is a shorthand for defining RESTful routes :
   # get "/articles", to: "articles#index"
